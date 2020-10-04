cc806bf909f8243b93e45f81ac9cb10fd4ed29c0
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-10-04 13:03:36 +0200

    working properly | added sticky, changed text, added clipboard function

--- src/modules/box_item.jsx
+++ src/modules/box_item.jsx
@@ -2,6 +2,25 @@ import React, { Component } from 'react'
 
 
 export default class BoxItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { copied: false, buttonClasses: "fa-clone far" }
+    }
+
+    handleCopy = (text) => {
+        navigator.clipboard.writeText(text)
+        this.setState({ ...this.setState, copied: true, buttonClasses: "fas fa-check" })
+    }
+
+    handleEnter = (e) => {
+        if (this.state.copied === true) return
+        this.setState({ ...this.state, buttonClasses: "fa-clone fas" })
+    }
+    handleLeave = (e) => {
+        if (this.state.copied === true) return
+        this.setState({ ...this.state, buttonClasses: "fa-clone far" })
+    }
+
     render() {
         const { name, description, code } = this.props.item
         // availabe: {author, categories, tags, version}
@@ -10,6 +29,9 @@ export default class BoxItem extends Component {
                 <h4 id="const" className="title is-3">{name}</h4>
                 <article className="message is-primary">
                     <span className="icon has-text-primary">
+
+                        <a onClick={() => this.handleCopy(code)} onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave} ><i className={this.state.buttonClasses}></i>
+                        </a>
                     </span>
                     <div className="message-body">
                         {description}
--- src/modules/categories.jsx
+++ src/modules/categories.jsx
@@ -12,7 +12,20 @@ export default class Categories extends Component {
                 [{ name: "CSS", logo: "fab fa-css3-alt" }, { name: "JS", logo: "fab fa-js" }, { name: "HTML", logo: "fab fa-html5" }],
             packages: [{ name: "abc", desc: "no description given", code: "#!/bin/bash\n\nfunction abc {\n\tSTRING=\"a b c d e f g i f g h i j k l m n o p q r s t u v w x y z\"\n\tfor CHAR in $STRING;do\n\t\tCOLOR=$((1 + RANDOM % 10))\n\t\tprintf \"%s\" \"$(tput setaf $COLOR)\" \"$CHAR\" \"$(tput sgr0) \"\n\tdone\n\techo\n}" }],
             queryCategory: "",
-            isQuerying: false
+            isQuerying: false,
+            menuClasses: ["is-medium menu"]
+        }
+
+    }
+
+    handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 400) {
+            this.setState({ ...this.state, menuClasses: ["is-medium menu scrolled"] })
+        }
+        else {
+            this.setState({ ...this.state, menuClasses: ["is-medium menu"] })
+
         }
     }
 
@@ -47,6 +60,7 @@ export default class Categories extends Component {
                 const oldState = { ...this.state }
                 this.setState({ ...oldState, categories: result })
             })
+        window.addEventListener("scroll", this.handleScroll)
     }
 
     componentDidUpdate() {
@@ -60,7 +74,7 @@ export default class Categories extends Component {
                 <div className="container">
                     <div className="columns">
                         <div className="column is-3">
-                            <aside className="is-medium menu">
+                            <aside className={this.state.menuClasses}>
                                 <p className="menu-label">
                                     categories
           </p>
@@ -68,7 +82,7 @@ export default class Categories extends Component {
                                     {this.state.categories.map((item, i) => <CategoryItem key={"category-" + i} item={item} handleChange={this.handleCategoryChange} />)}
                                 </ul>
                                 <p className="menu-label">
-                                    More to read...
+                                    Packages
               </p>
                                 <ul className="menu-list">
                                     {this.state.packages.map((item, i) => <PackageListItem key={"package--item-" + i} item={item} />)}
